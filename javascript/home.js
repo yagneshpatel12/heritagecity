d9366c0693c7fe37e2847482b899870b331a03ed
yagnesh <yagnesh6202patel@gmail.com>
2021-05-17 22:00:53 +0530

    making Responsive stage-1

--- javascript/home.js
+++ javascript/home.js
@@ -1,15 +1,18 @@
-var nav_without_login = document.getElementsByClassName('nav-without-login')[0];
+var navbar_section = document.getElementsByClassName('navbar-section')[0];
 var nav_color_change = document.getElementsByClassName('color-change')
 window.onscroll = function () { 
     "use strict";
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
-        nav_without_login.style.backgroundColor='white';
+        navbar_section.style.backgroundColor='white';
         for(let i=0;i<nav_color_change.length;i++)
-        nav_color_change[i].style.color='black'
+        nav_color_change[i].style.color='#424242';
     } 
     else {
-        nav_without_login.style.backgroundColor='transparent';
+        navbar_section.style.backgroundColor='transparent';
         for(let i=0;i<nav_color_change.length;i++)
         nav_color_change[i].style.color='white'
     }
 };
+let gallery_section = document.getElementsByClassName('carousel')[0];
+gallery_section.setAttribute('data-flickity','{ "freeScroll": true ,"wrapAround": true ,"autoPlay":true }');
+console.log(gallery_section.getAttribute('data-flickity')); 
